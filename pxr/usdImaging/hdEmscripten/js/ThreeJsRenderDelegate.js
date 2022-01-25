e5dea62ed17b03f54695ac17d8e7dc7ff634e3d3
frericp <philipp.frericks@autodesk.com>
2022-01-25 14:49:03 +0100

    initial js render delegate improvements

--- pxr/usdImaging/hdEmscripten/js/ThreeJsRenderDelegate.js
+++ pxr/usdImaging/hdEmscripten/js/ThreeJsRenderDelegate.js
@@ -6,12 +6,18 @@ class TextureRegistry {
   }
   getTexture(filename) {
     if (this.textures[filename]) {
-      return Promise.resolve(this.textures[filename]);
+      return this.textures[filename];
     }
 
-    let ressourePath = filename;
+    let textureResolve, textureReject;
+    this.textures[filename] = new Promise((resolve, reject) => {
+      textureResolve = resolve;
+      textureReject = reject;
+    });
+
+    let resourcePath = filename;
     if (filename[0] !== '/') {
-      ressourePath = this.basename + '[' + filename +']';
+      resourcePath = this.basename + '[' + filename +']';
     }
 
     let filetype = undefined;
@@ -25,43 +31,36 @@ class TextureRegistry {
       throw new Error('Unknown filetype');
     }
 
-    let promise;
-    window.driver.getFile(ressourePath, (loadedFile) => {
+    window.driver.getFile(resourcePath, (loadedFile) => {
       if (!loadedFile) {
-        throw new Error('Unknown file: ' + ressourePath)
+        textureReject(new Error('Unknown file: ' + resourcePath));
+        return;
       }
 
       let blob = new Blob([loadedFile.slice(0)], {type: filetype});
       let blobUrl = URL.createObjectURL(blob);
-      this.textures[filename] = {
-        blob: blob,
-        blobUrl: blobUrl,
-        matrix: new THREE.Matrix3()
-      }
-      promise = new Promise((resolve, reject) => {
-        // load a resource
-        this.loader.load(
+
+      // Load the texture
+      this.loader.load(
         // resource URL
         blobUrl,
-        //'./_BaseColor_0.png',
 
         // onLoad callback
         (texture) => {
-          this.textures[filename].texture = texture;
-          resolve(texture);
+          textureResolve(texture);
         },
 
-        // onProgress callback currently not supported
+        // onProgress callback currently not used
         undefined,
 
         // onError callback
-        ( err ) => {
-          reject(err);
+        (err) => {
+          textureReject(err);
         }
-        );
-      });
+      );
     });
-    return promise;
+
+    return this.textures[filename];
   }
 }
 
@@ -74,14 +73,15 @@ class HydraMesh {
     this._normals = undefined;
     this._indices = undefined;
 
-    const material = new THREE.MeshPhongMaterial( {
+    const material = new THREE.MeshPhysicalMaterial( {
       side: THREE.DoubleSide,
-      vertexColors: false//,
+      color: new THREE.Color(0x00ff00) // a green color to indicate a missing material
     } );
 
     this._mesh = new THREE.Mesh( this._geometry, material );
     window.scene.add(this._mesh); // FIXME
   }
+
   updateOrder(attribute, attributeName) {
     if (attribute && this._indices) {
       let values = [];
@@ -96,6 +96,7 @@ class HydraMesh {
       this._geometry.setAttribute( attributeName, new THREE.Float32BufferAttribute( values, 3 ) );
     }
   }
+
   updateIndices(indices) {
     this._indices = [];
     for (let i = 0; i< indices.length; i++) {
@@ -105,113 +106,103 @@ class HydraMesh {
     this.updateOrder(this._points, 'position');
     this.updateOrder(this._normals, 'normal');
   }
+
   setTransform(matrix) {
     this._mesh.matrix.set(...matrix);
     this._mesh.matrix.transpose();
     this._mesh.matrixAutoUpdate = false;
   }
+
   updateNormals(normals) {
-    //this._geometry.setAttribute( 'normal', new THREE.Float32BufferAttribute( normals, 3 ) );
     this._normals = normals.slice(0);
     this.updateOrder(this._normals, 'normal');
   }
+
   setMaterial(materialId) {
     console.log('Material: ' + materialId);
     if (this._interface.materials[materialId]) {
       this._mesh.material = this._interface.materials[materialId]._material;
     }
   }
+
   updatePrimvar(name, data, dimension) {
     if (name === 'points' || name === 'normals') {
       return;
     }
 
-    if (name === 'st') {
+    // TODO: Support multiple UVs. For now, we simply set uv = uv2, which is required when a material has an aoMap.
+    if (name.startsWith('st')) {
       name = 'uv';
     }
 
     console.log('Setting PrimVar: ' + name);
     this._geometry.setAttribute( name, new THREE.Float32BufferAttribute( data, dimension ) );
+    if (name === 'uv') {
+      this._geometry.attributes.uv2 = this._geometry.attributes.uv;
+    }
   }
+
   updatePoints(points) {
     this._points = points.slice(0);
     this.updateOrder(this._points, 'position');
   }
 
   commit() {
-    if(this._geomIsDirty) {
-        this._geomIsDirty = false
-        
-        const indices = this._attributes.index.data;
-        const geom = new THREE.BufferGeometry();
-        for (const attr in this._attributes) {
-
-            const indexedBuffer = this._attributes[attr].data
-            const dimensions = this._attributes[attr].dimensions
-
-            if(attr === 'position' || attr === 'normal')
-                indexedBuffer = this.updateOrder(indexedBuffer, indices);
-
-            switch (dimensions) {
-                case 3:
-                    geom.addAttribute(attr, new THREE.BufferAttribute(indexedBuffer, dimensions))
-                    break;
-                case 2:
-                    geom.addAttribute(attr, new THREE.BufferAttribute(indexedBuffer, dimensions))
-                    break;
-                case 1:
-                    // Do nothing for index buffers
-                    break
-            }
-        }
-        console.log("TODO - Geometry Update");
-        return; //         
-
-        if (!this._geomId) {
-            this._geom = geom;
-            this._geomId = this._interface.modelBuilder.addGeometry(this._geom)
-            this._fragId = this._interface.modelBuilder.addFragment(this._geomId, 'default')
-
-            this._interface.modelBuilder.changeFragmentGeometry(this._fragId, this._geom)
-        } else {
-            this._interface.modelBuilder.changeGeometry(this._geom, this._interface.modelBuilder.packNormals(geom));
-            this._geom = geom;
-        }
-    }
-
-    if(this._xfmIsDirty) {
-        this._xfmIsDirty = false
-        console.log("TODO - Transform Update");
-//        this._interface.modelBuilder.changeFragmentTransform(this._fragId, this._transform)
-    }
-
-    if(this._matIsDirty) {
-        this._matIsDirty = false
-        // Guessing if material is still default, we should switch fragment to use first official material
-        if(this._matName === 'default') {
-            for(let name in this._interface.materials) {
-                if(name !== 'default') {
-                    this._matName = name;
-                    break;
-                }
-            }
-        }
-        console.log("TODO - Material Update");
-//        this._interface.modelBuilder.changeFragmentMaterial(this._fragId, this._matName)
-    }
+    // Nothing to do here. All Three.js resources are already updated during the sync phase.
   }
 
 }
 
+let defaultMaterial;
+
 class HydraMaterial {
+  // Maps USD preview material texture names to Three.js MeshPhysicalMaterial names
+  static usdPreviewToMeshPhysicalTextureMap = {
+    'diffuseColor': 'map',
+    'clearcoat': 'clearcoatMap',
+    'clearcoatRoughness': 'clearcoatRoughnessMap',
+    'emissiveColor': 'emissiveMap',
+    'occlusion': 'aoMap',
+    'roughness': 'roughnessMap',
+    'metallic': 'metalnessMap',
+    'normal': 'normalMap',
+    'opacity': 'alphaMap'
+  };
+
+  static channelMap = {
+    // Three.js expects many 8bit values such as roughness or metallness in a specific RGB texture channel.
+    // We could write code to combine multiple 8bit texture files into different channels of one RGB texture where it
+    // makes sense, but that would complicate this loader a lot. Most Three.js loaders don't seem to do it either.
+    // Instead, we simply provide the 8bit image as an RGB texture, even though this might be less efficient.
+    'r': THREE.RGBFormat,
+    'rgb': THREE.RGBFormat,
+    'rgba': THREE.RGBAFormat
+  };
+
+  // Maps USD preview material property names to Three.js MeshPhysicalMaterial names
+  static usdPreviewToMeshPhysicalMap = {
+    'clearcoat': 'clearcoat',
+    'clearcoatRoughness': 'clearcoatRoughness',
+    'diffuseColor': 'color',
+    'emissiveColor': 'emissive',
+    'ior': 'ior',
+    'metallic': 'metalness',
+    'opacity': 'opacity',
+    'roughness': 'roughness',
+  };
+
   constructor(id, hydraInterface) {
     this._id = id;
     this._nodes = {};
     this._interface = hydraInterface;
-    this._material = new THREE.MeshPhongMaterial( {
+    if (!defaultMaterial) {
+      defaultMaterial = new THREE.MeshPhysicalMaterial({
         side: THREE.DoubleSide,
-        vertexColors: false
-      } );
+        color: new THREE.Color(0xff2997), // a bright pink color to indicate a missing material
+        envMap: window.envMap,
+      });
+    }
+    this._material = defaultMaterial.clone();
   }
 
   updateNode(networkId, path, parameters) {
@@ -219,26 +210,79 @@ class HydraMaterial {
     this._nodes[path] = parameters;
   }
 
-  assignTexture(mainMaterial, parameterName, materialParameterMapName, materialParameterName, parameterScaleValue) {
+  assignTexture(mainMaterial, parameterName) {
+    const materialParameterMapName = HydraMaterial.usdPreviewToMeshPhysicalTextureMap[parameterName];
+    if (materialParameterMapName === undefined) {
+      console.warn(`Unsupported material texture parameter '${parameterName}'.`);
+      return;
+    }
     if (mainMaterial[parameterName] && mainMaterial[parameterName].nodeIn) {
-      let textureFileName = mainMaterial[parameterName].nodeIn.file;
+      const textureFileName = mainMaterial[parameterName].nodeIn.file;
+      const channel = mainMaterial[parameterName].inputName;
+
+      // For debugging
+      const matName = Object.keys(this._nodes).find(key => this._nodes[key] === mainMaterial);
+      console.log(`Setting texture '${materialParameterMapName}' (${textureFileName}) of material '${matName}'...`);
+
       this._interface.registry.getTexture(textureFileName).then(texture => {
-        this._material[materialParameterMapName] = texture;
-        if (materialParameterName !== undefined) {
-          this._material[materialParameterName] = parameterScaleValue;
+        if (materialParameterMapName === 'alphaMap') {
+          // If this is an opacity map, check if it's using the alpha channel of the diffuse map.
+          // If so, simply change the format of that diffuse map to RGBA and make the material transparent.
+          // If not, we need to copy the alpha channel into a new texture's green channel, because that's what Three.js
+          // expects for alpha maps (not supported at the moment).
+          // NOTE that this only works if diffuse maps are always set before opacity maps, so the order of
+          // 'assingTexture' calls for a material matters.
+          if (textureFileName === mainMaterial.diffuseColor?.nodeIn?.file && channel === 'a') {
+            this._material.map.format = THREE.RGBAFormat;
+          } else {
+            // TODO: Extract the alpha channel into a new RGB texture.
+          }
+
+          this._material.transparent = true;
+          this._material.needsUpdate = true;
+          return;
+        } else if (materialParameterMapName === 'metalnessMap') {
+          this._material.metalness = 1.0;
+        } else if (materialParameterMapName === 'emissiveMap') {
+          this._material.emissive = new THREE.Color(0xffffff);
+        } else if (!HydraMaterial.channelMap[channel]) {
+          console.warn(`Unsupported texture channel '${channel}'!`);
+          return;
         }
+
+        // Clone texture and set the correct format.
+        const clonedTexture = texture.clone();
+        clonedTexture.format = HydraMaterial.channelMap[channel];
+        clonedTexture.needsUpdate = true;
+        this._material[materialParameterMapName] = clonedTexture;
+
         this._material.needsUpdate = true;
       });
     } else {
       this._material[materialParameterMapName] = undefined;
-      if (materialParameterName !== undefined) {
-        this._material[materialParameterName] = 0.0;
+    }
+  }
+
+  assignProperty(mainMaterial, parameterName) {
+    const materialParameterName = HydraMaterial.usdPreviewToMeshPhysicalMap[parameterName];
+    if (materialParameterName === undefined) {
+      console.warn(`Unsupported material parameter '${parameterName}'.`);
+      return;
+    }
+    if (mainMaterial[parameterName] !== undefined && !mainMaterial[parameterName].nodeIn) {
+      console.log(`Assigning property ${parameterName}: ${mainMaterial[parameterName]}`);
+      if (Array.isArray(mainMaterial[parameterName])) {
+        this._material[materialParameterName] = new THREE.Color().fromArray(mainMaterial[parameterName]);
+      } else {
+        this._material[materialParameterName] = mainMaterial[parameterName];
+        if (materialParameterName === 'opacity' && mainMaterial[parameterName] < 1.0) {
+          this._material.transparent = true;
+        }
       }
     }
   }
 
   updateFinished(type, relationships) {
-    //return;
     for (let relationship of relationships) {
       relationship.nodeIn = this._nodes[relationship.inputId];
       relationship.nodeOut = this._nodes[relationship.outputId];
@@ -256,32 +300,43 @@ class HydraMaterial {
       }
     }
 
+    // TODO: Ideally, we don't recreate the material on every update.
+    // Creating a new one requires to also update any meshes that reference it. So we're relying on the C++ side to
+    // call this before also calling `setMaterial` on the affected meshes.
     if (!mainMaterialNode) {
-      this._material = new THREE.MeshPhongMaterial( {
-        side: THREE.DoubleSide,
-        vertexColors: false
-      } );
+      this._material = defaultMaterial.clone();
       return;
     }
 
     console.log('Creating Material: ' + this._id);
-    this._material = new THREE.MeshPhysicalMaterial({
-    });
-    this.assignTexture(mainMaterialNode, 'diffuseColor', 'map');
-    this.assignTexture(mainMaterialNode, 'clearcoat', 'clearcoatMap', 'clearcoat', 1.0);
-    this.assignTexture(mainMaterialNode, 'clearcoatRoughness', 'clearcoatRoughnessMap', 'clearcoatRoughness', 5.0);
-    this.assignTexture(mainMaterialNode, 'roughness', 'roughnessMap', 'roughness', 1.5);
-    this.assignTexture(mainMaterialNode, 'metallic', 'metalnessMap', 'metalness', 1.0);
-    this.assignTexture(mainMaterialNode, 'normal', 'normalMap');
+    this._material = new THREE.MeshPhysicalMaterial({});
+
+    // Assign textures
+    for (let key in HydraMaterial.usdPreviewToMeshPhysicalTextureMap) {
+      this.assignTexture(mainMaterialNode, key);
+    }
+
+    // Assign material properties
+    for (let key in HydraMaterial.usdPreviewToMeshPhysicalMap) {
+      this.assignProperty(mainMaterialNode, key);
+    }
+
+    if (window.envMap) {
+      this._material.envMap = window.envMap;
+    }
+
+    console.log(this._material);
   }
 }
 
 export class RenderDelegateInterface {
+
   constructor(filename) {
     this.registry = new TextureRegistry(filename);
     this.materials = {};
     this.meshes = {};
   }
+
   createRPrim(typeId, id, instancerId) {
     console.log('Creating RPrim: ' + typeId + ' ' + id);
     let mesh = new HydraMesh(id, this);
@@ -307,6 +362,7 @@ export class RenderDelegateInterface {
       return undefined;
     }
   }
+
   CommitResources() {
     for (const id in this.meshes) {
         const hydraMesh = this.meshes[id]
